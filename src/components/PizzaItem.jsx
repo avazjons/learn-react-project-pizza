42ef401ea9dda30d0c75335a8f17968f9d986f15
Soliev Avazjon <soliev.avazjon@gmail.com>
2023-01-12 18:38:02 +0700

    learn useState and counter

--- src/components/PizzaItem.jsx
+++ src/components/PizzaItem.jsx
@@ -1,4 +1,12 @@
-export default function PizzaItem(props) {
+import { useState } from "react";
+
+export default function PizzaItem({ title, price }) {
+    const [pizzaCount, setPizzaCount] = useState(0);
+
+    const handleAddButton = () => {
+        setPizzaCount(pizzaCount + 1);
+    }
+
     return (
         <div className="pizza-block">
             <img
@@ -6,7 +14,7 @@ export default function PizzaItem(props) {
                 src="https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg"
                 alt="Pizza"
             />
-            <h4 className="pizza-block__title">{props.title}</h4>
+            <h4 className="pizza-block__title">{title}</h4>
             <div className="pizza-block__selector">
                 <ul>
                     <li className="active">тонкое</li>
@@ -19,8 +27,8 @@ export default function PizzaItem(props) {
                 </ul>
             </div>
             <div className="pizza-block__bottom">
-                <div className="pizza-block__price">от {props.price} ₽</div>
-                <div className="button button--outline button--add">
+                <div className="pizza-block__price">от {price} ₽</div>
+                <button onClick={handleAddButton} className="button button--outline button--add">
                     <svg
                         width="12"
                         height="12"
@@ -34,8 +42,8 @@ export default function PizzaItem(props) {
                         />
                     </svg>
                     <span>Добавить</span>
-                    <i>2</i>
-                </div>
+                    <i>{pizzaCount}</i>
+                </button>
             </div>
         </div>
     )
