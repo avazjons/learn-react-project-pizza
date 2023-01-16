91f3a4b8fe4010ef1f68249412d0c6532aadcc0a
Soliev Avazjon <soliev.avazjon@gmail.com>
2023-01-16 13:25:37 +0700

    component Categories

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,13 +1,28 @@
+import React, { useState } from 'react';
+
 export default function Categories() {
+    const [activeIndex, setActiveIndex] = useState(0);
+    const categories = [
+        { id: 0, name: "Все" },
+        { id: 1, name: "Мясные" },
+        { id: 2, name: "Вегетарианская" },
+        { id: 3, name: "Гриль" },
+        { id: 4, name: "Острые" },
+        { id: 5, name: "Закрытые" },
+    ];
+
+    const activeHandle = (index) => {
+        setActiveIndex(index)
+    }
+
     return (
         <div className="categories">
             <ul>
-                <li className="active">Все</li>
-                <li>Мясные</li>
-                <li>Вегетарианская</li>
-                <li>Гриль</li>
-                <li>Острые</li>
-                <li>Закрытые</li>
+                {
+                    categories.map((value) => {
+                        return <li onClick={() => activeHandle(value.id)} className={activeIndex === value.id ? "active" : ""} key={value.id}>{value.name}</li>
+                    })
+                }
             </ul>
         </div>
     )
